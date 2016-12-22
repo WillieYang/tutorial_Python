1d34ae94268e7c1bfd8316ec28ee248b15246e1b
WillieYang <849346604@qq.com>
2016-12-22 21:17:42 +0000

    a comparision function is added

--- tutorial2.py
+++ tutorial2.py
@@ -1,5 +1,13 @@
-n = 1;
-sum = 0;
-while n > 0:
-	sum = sum + n
-print sum
+def comparisation(x, y):
+	if x == y :
+		print 'The numbers you have input is equal'
+	elif x > y:
+		print 'The %d is bigger than %d' % (x, y)
+	else:
+		print 'The %d is smaller than %d' % (x, y)
+
+print 'Please input two numbers to compare:'
+x = int(raw_input())
+y = int(raw_input())
+comparisation(x, y)
+
