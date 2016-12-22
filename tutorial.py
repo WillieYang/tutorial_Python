7d66d4867c9e8f5b42adc41d100b369ba7fc2f2c
WillieYang <849346604@qq.com>
2016-12-22 16:07:12 +0000

    First virsion

---
+++ tutorial.py
@@ -0,0 +1,22 @@
+# print 'Please enter your name:'
+# name = raw_input()
+# print 'Welcome the world of pthon: %s' % name
+# print 'Please enter a number:'
+# number = int(raw_input())
+# number = input()
+# if number > 50:
+# 	print 'The number you have input is bigger than 50'
+# elif number >=25 and number <=50 :
+# 	print 'The number you have input is between 25 and 50'
+# else:
+# 	print 'The number you have input is smaller than 25'
+# sum = 0
+# for n in range(101):
+# 	sum = n + sum
+# print sum
+# sum = 0
+# n = 100
+# while n > 0:
+# 	n = n - 2
+# 	sum = sum + n
+# print sum
