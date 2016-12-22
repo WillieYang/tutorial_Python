d4ea71ddb39057fd537f5e701a9e85c33d5363f3
WillieYang <849346604@qq.com>
2016-12-22 22:23:30 +0000

    a iteration function

--- tutorial2.py
+++ tutorial2.py
@@ -14,6 +14,14 @@
 
 # l = [2, 3, 5, 'sheng', 'yang']
  
-def parameter(*l, **dic):
-	print 'l', l,'dic', dic
-parameter(1, 3, 5, life = 'sheng', will = 'yang')
+# def parameter(a, b, *l, **dic):
+# 	print 'int:',a , b, 'l:', l,'dic:', dic
+# parameter(1, 3, 5, life = 'sheng', will = 'yang')
+
+def iteration(n):
+	if n == 1:
+		return 1
+	return n * iteration(n-1)
+
+f = iteration(5)
+print 'The result is', f
