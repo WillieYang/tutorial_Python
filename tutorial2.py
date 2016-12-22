751685048a933fe0a8e7755e81d2569605dc9d4d
WillieYang <849346604@qq.com>
2016-12-22 22:00:51 +0000

    a fucntion whose parameter can be a bunch of list or tuple or dictionary

--- tutorial2.py
+++ tutorial2.py
@@ -1,13 +1,19 @@
-def comparisation(x, y):
-	if x == y :
-		print 'The numbers you have input is equal'
-	elif x > y:
-		print 'The %d is bigger than %d' % (x, y)
-	else:
-		print 'The %d is smaller than %d' % (x, y)
+# def comparisation(x, y):
+# 	if x == y :
+# 		print 'The numbers you have input is equal'
+# 	elif x > y:
+# 		print 'The %d is bigger than %d' % (x, y)
+# 	else:
+# 		print 'The %d is smaller than %d' % (x, y)
 
-print 'Please input two numbers to compare:'
-x = int(raw_input())
-y = int(raw_input())
-comparisation(x, y)
+# print 'Please input two numbers to compare:'
+# x = int(raw_input())
+# y = int(raw_input())
+# l = [x, y]
+# comparisation(x, y)
 
+# l = [2, 3, 5, 'sheng', 'yang']
+ 
+def parameter(*l, **dic):
+	print 'l', l,'dic', dic
+parameter(1, 3, 5, life = 'sheng', will = 'yang')
